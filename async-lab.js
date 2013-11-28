8e38777a5a23da5b01c7c97fb1595a5eb18f5413
xjj59307 <xjj59307@gmail.com>
2013-11-29 01:19:32 +0900

    async-lab.js

--- async-lab.js
+++ async-lab.js
@@ -1,6 +1,21 @@
 async = require('async');
 fs = require('fs');
 
+var print = function(value, callback) {
+    setTimeout(function() {
+        console.log(value);
+        callback();
+    }, 2000);
+};
+
+async.series([
+    print.bind(undefined, "first"),
+    print.bind(undefined, "second"),
+    print.bind(undefined, "third"),
+    print.bind(undefined, "fourth"),
+    print.bind(undefined, "fifth")
+]);
+
 async.map(
     ['./README.md', './step-lab.js', './q-lab.js'],
     fs.readFile,
@@ -8,4 +23,3 @@ async.map(
         console.log(files.join('\n'));
     } 
 );
-
